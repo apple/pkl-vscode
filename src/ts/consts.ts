2bc23bbedc3c84b695221ddc1410ec82eddc0b4a
Daniel Chao <daniel.h.chao@gmail.com>
2024-10-10 09:04:33 -0700

    Bump bundled LSP to 0.1.1 (#32)

--- src/ts/consts.ts
+++ src/ts/consts.ts
@@ -38,7 +38,7 @@ export const COMMAND_OPEN_WORKSPACE_SETTINGS = "workbench.action.openSettings";
 
 export const COMMAND_RELOAD_WORKSPACE_WINDOW = "workbench.action.reloadWindow";
 
-export const BUNDLED_LSP_VERSION = "0.1.0";
+export const BUNDLED_LSP_VERSION = "0.1.1";
 
 /**
  * The directory that pkl-lsp distributions get saved to.
