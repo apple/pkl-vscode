93d25a0669b70963c1a3adc51f2cb072d20b66df
Daniel Chao <daniel.h.chao@gmail.com>
2025-04-22 08:00:51 -0700

    Bump bundled pkl-lsp version, consider 0.+ versions compatible (#54)
    This bumps pkl-lsp to 0.3.0.
    Additionally, it considers minor version bumps of 0.x versions as
    compatible.
    
    This means that new release of pkl-lsp can be automatically downloaded.
    One implication here is that we cannot introduce breaking changes to
    the LSP without releasing a new major version.

--- src/ts/consts.ts
+++ src/ts/consts.ts
@@ -42,7 +42,7 @@ export const COMMAND_OPEN_WORKSPACE_SETTINGS = "workbench.action.openSettings";
 
 export const COMMAND_RELOAD_WORKSPACE_WINDOW = "workbench.action.reloadWindow";
 
-export const BUNDLED_LSP_VERSION = "0.1.1";
+export const BUNDLED_LSP_VERSION = "0.3.1";
 
 /**
  * The directory that pkl-lsp distributions get saved to.
